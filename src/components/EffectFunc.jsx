38befaf6f558930b9c7fd48b0994b83c63e6f4bd
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-05-10 12:17:03 +0900

    comment out

--- src/components/EffectFunc.jsx
+++ src/components/EffectFunc.jsx
@@ -14,12 +14,18 @@ const useStyles = makeStyles((theme) => ({
 
 const EffectFunc = () => {
   const classes = useStyles();
+  //countの初期値は0,setCountで再定義
   const [count, setCount] = useState(0)
+  //nameの初期値はlastName,firstNameともに空文字,setNameで再定義
   const [name, setName] = useState({
     lastName: '',
     firstName: ''
   })
+  //useEffectは毎回のレンダリング後に呼ばれる。
+    //第1引数を副作用関数とよぶ。
+    //第2引数に比較するものを与える。前回と異なれば副作用関数を実行
   useEffect(() => {
+    //titleを変更している
     document.title =`${count}回クリックされました`
   },[count])
 
@@ -27,7 +33,8 @@ const EffectFunc = () => {
     <>
       <p>{`${count}回クリックされました`}</p>
       <ButtonGroup color="primary" aria-label="outlined primary button group">
-        <Button onClick={()=>setCount((prev) => prev + 1)}>
+        <Button onClick={()=>setCount(count + 1)}>
+        {/* <Button onClick={()=>setCount((prev) => prev + 1)}> */}
           ボタン
         </Button>
         <Button onClick={()=>setCount(0)}>
